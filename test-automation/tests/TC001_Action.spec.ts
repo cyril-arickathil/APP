3049eb9460d0dfe73e2b42225e22eaf1fd3ec730
rani <ranijasminsabat359@gmail.com>
2025-11-19 18:54:40 +0530

    added test for basic form

--- test-automation/tests/TC001_Action.spec.ts
+++ test-automation/tests/TC001_Action.spec.ts
@@ -13,6 +13,7 @@ const chkbox = page.locator('label').filter({ hasText: 'Remember me' }).first()
 await (chkbox).check();
 await expect (chkbox).toBeChecked();
 const submitbtn= page.locator('button').nth(3)
+await expect(submitbtn).toBeEnabled();
 await(submitbtn).click();
 })
 test("Using the grid",async({page})=>{
@@ -24,6 +25,16 @@ await page.locator("#inputPassword2").fill("Test345");
 const radiobtn = page.locator('nb-card nb-radio :text-is("Option 1")')
 await(radiobtn).click();
 await expect(radiobtn).toBeChecked();
+})
+test("Basic form",async({page})=>{
+await page.goto("http://localhost:4200/")
+await page.getByText("Forms").click();
+await page.getByText("Form Layouts").click();
+await page.locator("#exampleInputEmail1").fill("abc123@gmail.com");
+await page.locator("#exampleInputPassword1").fill("test398");
+const chk = page.locator('label').filter({ hasText: 'Check me out' })
+await(chk).check();
+await expect(chk).toBeChecked();
 })
 
 
