126fe2e0bca4a1f9a4c0a6433f4df08768eb5fb5
Madhu <m.madhu1256@gmail.com>
2025-11-19 07:49:02 +0530

    Update package dependencies and clean up test file

---
+++ test-automation/tests/Sample.spec.ts
@@ -0,0 +1,33 @@
+import {test} from '@playwright/test';
+
+test.beforeEach(async ({ page}) => {
+  await page.goto('http://localhost:4200/');
+  await page.setViewportSize({width: 1900, height: 980});
+  await page.waitForTimeout(3000); // Wait for 3 seconds
+});
+//test
+
+test('Forms',async ({page}) =>
+{    
+    
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click();
+    
+    await page.waitForTimeout(3000); // Wait for another 3 seconds
+    await page.getByRole('textbox', { name: 'Email'}).nth(0).fill('m.madhu1256@gmail.com');
+    await page.getByRole('textbox', { name: 'Password'}).fill('Madhu@1256');
+    await page.waitForTimeout(3000); // Wait for another 3 seconds
+    await page.close(); // Close the specific page
+}    
+);
+
+test('Charts',async ({page}) =>
+{    
+    
+    await page.locator('a[title="Charts"]').click();
+    await page.getByText('Echarts').click();
+    
+    await page.waitForTimeout(3000); // Wait for another 3 seconds
+    await page.close(); // Close the specific page
+}    
+);
